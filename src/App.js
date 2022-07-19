6096c6925835be1c3082eaffae0bf55bb29a8f71
juliajulia89 <julkajanicka@gmail.com>
2022-07-19 18:07:20 +0200

    structuring

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 
 import './App.css';
+import Header from "./components/Header"
+import Footer from "./components/Footer";
+import SectionOne from "./components/SectionOne";
+import SectionTwo from "./components/SectionTwo";
+import SectionThree from "./components/SectionThree";
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <SectionOne />
+      <SectionTwo />
+      <SectionThree />
+      <Footer />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { extendTheme, ChakraProvider } from "@chakra-ui/react";
+import  { theme }  from "./theme/theme";
+const { breakpoints, colors, space, radii, shadows, textStyles, components } =
+  theme;
 
+const impulsumTheme = extendTheme({
+  breakpoints,
+  colors,
+  space,
+  radii,
+  shadows,
+  textStyles,
+  components,
+});
 
+export default theme;
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <ChakraProvider theme={impulsumTheme}>
     <App />
-  </React.StrictMode>
+  </ChakraProvider>
 );
-
