e43db8233c9eef70d7d66dd9836b7da1463a783a
juliajulia89 <julkajanicka@gmail.com>
2022-07-19 18:19:12 +0200

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 
 import './App.css';
+import Navbar from "./components/Navbar";
 import Header from "./components/Header"
 import Footer from "./components/Footer";
 import SectionOne from "./components/SectionOne";
 import SectionTwo from "./components/SectionTwo";
 import SectionThree from "./components/SectionThree";
-
+import SectionFour from "./components/SectionFour";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Header />
       <SectionOne />
       <SectionTwo />
       <SectionThree />
+      <SectionFour />
       <Footer />
     </div>
   );
