21fe0aa6cf569b0d8445ca2d4f2a7c23b6edc34a
juliajulia89 <julkajanicka@gmail.com>
2022-07-23 16:29:02 +0200

    sectio four change

--- src/components/SectionFour.js
+++ src/components/SectionFour.js
@@ -1,9 +1,10 @@
-import { Box, Text, Button, Image, Link } from "@chakra-ui/react";
+import { Box, Text, Button, Image, Link, Flex } from "@chakra-ui/react";
 import imageFigma from "../images/Figma.svg"
 import imageGithub from "../images/Github.svg"
 
 const SectionFour = () => {
   return (
+   <>
     <Box pb="spacer-32">
       <Box
         background="text-button-accent"
@@ -25,28 +26,27 @@ const SectionFour = () => {
         >
           Request your free demo 
         </Button>
+        </Box>
+        </Box>
+        
+        <Box>
+         <Flex justifyContent="space-evenly" flexDirection={['column', 'row']} mr={['spacer-0', "spacer-28"]} textAlign='start'>
          <Box width={["null", "20%"]} ml={["spacer-8", "spacer-0"]}>
           <Image src={imageGithub} alt="GithubLogo" />
           <Text textStyle="text-header-L" pt="spacer-4">
             Composer is open-sourced on GitHub. You're welcome to contribute.
           </Text>
-          <Button
-            borderRadius="radii-6"
-            color="accent-default"
-            mt="spacer-4"
-            background="text-button-accent"
-          >
-            Contribute on GitHub
-            
+          <Button borderRadius="radii-6" color="accent-default" mt="spacer-4" background="text-button-accent" >
+            Contribute on GitHub  
           </Button>
         </Box>
         <Box
           width={["null", "20%"]}
-          ml={["spacer-8", "spacer-0"]}
-          mt={["spacer-8", "spacer-0"]}
+          ml={["spacer-0", "spacer-0"]}
+          mt={["spacer-0", "spacer-0"]}
         >
-          <Image src={imageFigma} alt="Figmalogo" />
-          <Text textStyle="text-header-L" pt="spacer-4">
+          <Image src={imageFigma} alt="FigmaLogo" />
+          <Text textStyle="text-header-L" pt="spacer-3">
             Composer is part of the Figma community. Visit us with handle
             @impulsum
           </Text>
@@ -78,9 +78,9 @@ const SectionFour = () => {
             <Link>Meet the crew on Instagram</Link>
           </Text>
         </Box>
-      
-      </Box>
-    </Box>
+      </Flex>
+        </Box>
+        </>
   );
 };
 
